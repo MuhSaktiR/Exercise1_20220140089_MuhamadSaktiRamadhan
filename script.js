900c0ac55d74ab8958815eddc4e4f870f60d3605
MuhSaktiR <seken876@gmail.com>
2024-03-29 18:56:32 +0700

    Exercise-1

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil(){
+        alert("Arang adalah bukti bahwa dari proses pembakaran yang melanda, bisa timbul kegunaan yang membentuk dan menyucikan.")
+    }
+
+    function show(){
+        alert("Seperti arang yang mampu menyaring kegelapan, begitulah kita bisa menjadikan setiap cobaan sebagai pelajaran yang memurnikan jiwa.")
+    }
+}
